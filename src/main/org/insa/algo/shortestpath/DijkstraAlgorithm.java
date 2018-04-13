63ee60f59a153e8f9f43a15aec737248d257ef12
M. Biers Maxence <biers@insa-10226>
2018-04-13 16:54:02 +0200

    Ebauche label/djikstra

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -1,7 +1,16 @@
 package org.insa.algo.shortestpath;
 
+import java.util.ArrayList;
+import org.insa.algo.AbstractAlgorithm;
+import org.insa.algo.utils.BinaryHeap;
+import org.insa.graph.*;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
+	
+	BinaryHeap labels = new BinaryHeap();
+	
+	
     public DijkstraAlgorithm(ShortestPathData data) {
         super(data);
     }
@@ -10,7 +19,13 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     protected ShortestPathSolution doRun() {
         ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
+        Graph graph = data.getGraph();
         // TODO:
+
+        for (Node n : graph) {
+        	
+        	
+        }
         return solution;
     }
 
---
+++ src/main/org/insa/algo/shortestpath/Label.java
@@ -0,0 +1,41 @@
+
+package org.insa.algo.shortestpath;
+import org.insa.algo.AbstractAlgorithm;
+import org.insa.graph.*;
+
+
+public class Label{
+	private double cost;
+	private Node father;
+	private int mark;
+	private Node node;
+	
+	public Label(Node node, Node father) {
+		this.node = node;
+		this.father = father;
+		this.cost = Double.POSITIVE_INFINITY; //a changer
+		this.mark = 0;
+	}
+	
+	public getNode() {
+		
+	}
+	
+	public getFather() {
+		
+	}
+
+	public getCost() {
+		
+	}
+	public getMark() {
+		
+	}
+	public setCost() {
+		
+	}
+	public setMark() {
+		
+	}
+	
+}
