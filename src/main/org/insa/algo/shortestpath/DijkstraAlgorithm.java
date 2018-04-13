8a303ac437e4b8f2f91fa86f08e422b7d3386404
M. Biers Maxence <biers@insa-10226>
2018-04-13 18:13:50 +0200

    djikstra

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -8,8 +8,8 @@ import org.insa.graph.*;
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
 	
-	BinaryHeap labels = new BinaryHeap();
 	
+
 	
     public DijkstraAlgorithm(ShortestPathData data) {
         super(data);
@@ -17,15 +17,31 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     @Override
     protected ShortestPathSolution doRun() {
+    	BinaryHeap<Label> tas = new BinaryHeap();
+    	ArrayList<Label> labels = new ArrayList<Label>();
+    	
+    	Label temp  = new Label();//RAJOUTER CONSTRUCTEUR VIDE AVEC NODE A NULL
+    	
         ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
         Graph graph = data.getGraph();
         // TODO:
 
+        
+        //INITIALISATION
         for (Node n : graph) {
-        	
-        	
+        	if (n.equals(data.getOrigin())) {
+           		labels.add(new Label(n,0)); 
+           		tas.insert(new Label(n,0));
+        	}
+        	else {
+        		labels.add(new Label(n));        		
+        	}
         }
+        for(int i=0;i<NOMBRENOEUDS,i++){
+        	//SUITE
+        }
+        //LE RECONFORT
         return solution;
     }
 
